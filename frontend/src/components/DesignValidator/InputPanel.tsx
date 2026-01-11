fb112adb8285827ba4b41be94e7f4efa874ecfaf
Viswanathan R <nviswa192@gmail.com>
2026-01-11 15:40:20 +0530

    Cleanup unused props in InputPanel

--- frontend/src/components/DesignValidator/InputPanel.tsx
+++ frontend/src/components/DesignValidator/InputPanel.tsx
@@ -8,8 +8,6 @@ interface InputPanelProps {
     onValidate: (data: any) => void;
     loading: boolean;
     initialData?: any;
-    expanded?: boolean;
-    onToggleExpand?: () => void;
 }
 
 export default function InputPanel({ onValidate, loading, initialData }: InputPanelProps) {
