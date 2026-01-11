a17ec23a6debcba820406a1f0ce9c87484c4edd0
Viswanathan R <nviswa192@gmail.com>
2026-01-11 15:19:46 +0530

    Fixed the Delete Option

--- frontend/src/app/design-validator/page.tsx
+++ frontend/src/app/design-validator/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 
 import React from 'react';
 import { useAuth, UserButton, SignIn } from '@clerk/nextjs';
--- frontend/src/components/DesignValidator/ResultsPanel.tsx
+++ frontend/src/components/DesignValidator/ResultsPanel.tsx
@@ -1,3 +1,4 @@
+'use client';
 
 import React from 'react';
 import { DataGrid, GridColDef, GridRenderCellParams } from '@mui/x-data-grid';
